c805cd3132cfaf516b4ff9e55730931ce577057f
Seth Torek <seth.torek@gmail.com>
2021-04-07 21:57:43 -0400

    finished question objects

--- index.js
+++ index.js
@@ -8,18 +8,48 @@ inquirer
   .prompt([
     {
       type: 'input',
+      message: 'What is your GitHub username?',
+      name: 'username',
+    },
+    {
+      type: 'input',
+      message: 'What is your email address?',
+      name: 'email',
+    },
+    {
+      type: 'input',
       message: 'What is your project name?',
       name: 'project',
     },
     {
-      type: 'input',
-      message: 'What is your name',
-      name: 'name',
+        type: 'input',
+        message: 'Please write a short description of your project.',
+        name: 'description',
     },
     {
-      type: 'input',
-      message: 'how old are you',
-      name: 'age',
+        type: 'input',
+        message: 'what kind of license should your project have?',
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'what what command should be run to install dependencies?',
+        name: 'install',
+    },
+    {
+        type: 'input',
+        message: 'what command should be run to run tests?',
+        name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'what does the user need to know about using the repo?',
+        name: 'know',
+    },
+    {
+        type: 'input',
+        message: 'what does the user need to know about contributing to the repo?',
+        name: 'contribute',
     },
   ])
   .then((response) => {
@@ -30,13 +60,15 @@ inquirer
       })
     });
     // !!!!!!! json.stringify not working!!!!!!!!!!!!!!
+    // do i need the other questions in the screenshot?????????????????
+    // getting a null
 
 
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
 
-// // TODO: Create a function to initialize app
-// function init() {}
+// TODO: Create a function to initialize app
+function init() {}
 
-// // Function call to initialize app
-// init();
+// Function call to initialize app
+init();
